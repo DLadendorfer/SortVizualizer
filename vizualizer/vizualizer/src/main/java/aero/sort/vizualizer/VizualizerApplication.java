353b6c0eb77135f363f982937171d872735193d9
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-03-20 21:54:27 +0100

    added basic project structure

---
+++ vizualizer/vizualizer/src/main/java/aero/sort/vizualizer/VizualizerApplication.java
@@ -0,0 +1,13 @@
+package aero.sort.vizualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizualizerApplication.class, args);
+	}
+
+}
---
+++ vizualizer/vizualizer/src/test/java/aero/sort/vizualizer/VizualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package aero.sort.vizualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
