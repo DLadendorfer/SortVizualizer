2fc8117136a65daddb9a907f19a1ce43210097c4
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-03-25 20:14:02 +0100

    Setup slf4j with log4j

--- src/main/java/aero/sort/vizualizer/VizualizerApplication.java
+++ src/main/java/aero/sort/vizualizer/VizualizerApplication.java
@@ -1,9 +1,13 @@
 package aero.sort.vizualizer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class VizualizerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		Logger logger = LoggerFactory.getLogger(VizualizerApplication.class);
+		logger.info("Hello, World!");
 	}
 
 }
