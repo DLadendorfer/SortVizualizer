bca2cb7d4427f08716e90fae7ba37a4f62827359
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-04-14 10:35:17 +0200

    Bumped Mockito to 5.3.0; Added additional tests

---
+++ src/test/java/aero/sort/vizualizer/algorithms/concrete/QuickSortTest.kt
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------------
+// Copyright (c) Ladendorfer Daniel.
+// All Rights Reserved.  See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------
+package aero.sort.vizualizer.algorithms.concrete
+
+import aero.sort.vizualizer.algorithms.AbstractSortingAlgorithmTest
+import aero.sort.vizualizer.algorithms.ISortingAlgorithm
+import org.junit.jupiter.api.Disabled
+
+/**
+ * Tests [QuickSort].
+ *
+ * @author Daniel Ladendorfer
+ */
+@Disabled
+internal class QuickSortTest : AbstractSortingAlgorithmTest() {
+    override fun getAlgorithm(): ISortingAlgorithm {
+        return QuickSort()
+    }
+}
---
+++ src/test/java/aero/sort/vizualizer/app/ApplicationTest.kt
@@ -0,0 +1,25 @@
+// -------------------------------------------------------------------------------
+// Copyright (c) Ladendorfer Daniel.
+// All Rights Reserved.  See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------
+package aero.sort.vizualizer.app
+
+import aero.sort.vizualizer.VizualizerApplication
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+/**
+ * Tests [Application].
+ *
+ * @author Daniel Ladendorfer
+ */
+internal class ApplicationTest {
+
+    @Test
+    fun `Launch does not throw any exception`() {
+        assertDoesNotThrow(
+                { "No exception should occur" },
+                { Application.launch() }
+        )
+    }
+}
