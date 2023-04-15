fd23f01270c46af347fda221571779763c2c897a
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-04-15 20:50:22 +0200

    Static mocking UIBindings to fix github CI

--- src/test/java/aero/sort/vizualizer/VizualizerApplicationTest.kt
+++ src/test/java/aero/sort/vizualizer/VizualizerApplicationTest.kt
@@ -4,20 +4,29 @@
 // -------------------------------------------------------------------------------
 package aero.sort.vizualizer
 
+import aero.sort.vizualizer.ui.laf.UIBindings
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
 /**
  * Tests [VizualizerApplication].
  *
  * @author Daniel Ladendorfer
  */
+@ExtendWith(MockitoExtension::class)
 internal class VizualizerApplicationTest {
 
     @Test
     fun `Invokes main and simply checks that no exception is thrown`() {
-        assertDoesNotThrow {
-            VizualizerApplication.main(emptyArray())
+        val uiBindings = Mockito.mockStatic(UIBindings::class.java)
+        uiBindings.use {
+            uiBindings.`when`<UIBindings> { UIBindings.setupLookAndFeel() }.then { }
+            assertDoesNotThrow {
+                VizualizerApplication.main(emptyArray())
+            }
         }
     }
 }
--- src/test/java/aero/sort/vizualizer/app/ApplicationTest.kt
+++ src/test/java/aero/sort/vizualizer/app/ApplicationTest.kt
@@ -4,21 +4,30 @@
 // -------------------------------------------------------------------------------
 package aero.sort.vizualizer.app
 
+import aero.sort.vizualizer.ui.laf.UIBindings
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
 /**
  * Tests [Application].
  *
  * @author Daniel Ladendorfer
  */
+@ExtendWith(MockitoExtension::class)
 internal class ApplicationTest {
 
     @Test
     fun `Launch does not throw any exception`() {
-        assertDoesNotThrow(
-            { "No exception should occur" },
-            { Application.launch() }
-        )
+        val uiBindings = Mockito.mockStatic(UIBindings::class.java)
+        uiBindings.use {
+            uiBindings.`when`<UIBindings> { UIBindings.setupLookAndFeel() }.then { }
+            assertDoesNotThrow(
+                { "No exception should occur" },
+                { Application.launch() }
+            )
+        }
     }
 }
