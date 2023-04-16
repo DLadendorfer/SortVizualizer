9df7f5416d275fe22f80b0459c2c42671d7edc01
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-04-16 14:52:47 +0200

    Added plain color tests

---
+++ src/test/java/aero/sort/vizualizer/data/options/styles/concrete/WhiteTest.kt
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------------
+// Copyright (c) Ladendorfer Daniel.
+// All Rights Reserved.  See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------
+package aero.sort.vizualizer.data.options.styles.concrete
+
+import aero.sort.vizualizer.data.options.styles.AbstractPlainStyle
+import aero.sort.vizualizer.data.options.styles.AbstractPlainStyleTest
+import aero.sort.vizualizer.ui.constants.Theme
+import java.awt.Color
+
+/**
+ * Tests [White].
+ *
+ * @author Daniel Ladendorfer
+ */
+internal class WhiteTest : AbstractPlainStyleTest() {
+    override fun getExpectedColor(): Color = Theme.WHITE
+    override fun getPlainStyle(): AbstractPlainStyle = White()
+}
