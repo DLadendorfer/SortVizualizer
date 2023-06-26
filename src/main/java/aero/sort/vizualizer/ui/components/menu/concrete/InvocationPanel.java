69a39e002c8312803bdce8b798da23086a8d6440
Daniel Ladendorfer <daniel.ladendorfer@gmail.com>
2023-06-26 21:45:40 +0200

    Refactored InvocationPanel

--- src/main/java/aero/sort/vizualizer/ui/components/menu/concrete/InvocationPanel.java
+++ src/main/java/aero/sort/vizualizer/ui/components/menu/concrete/InvocationPanel.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.util.function.Consumer;
 
 /**
  * Panel that manages all sort action invocations.
@@ -20,26 +21,31 @@ import javax.swing.*;
  */
 public class InvocationPanel extends JPanel {
     private static final Logger logger = LoggerFactory.getLogger(InvocationPanel.class);
+    private static final String SORT_LABEL = "Sort";
+    private static final String STEP_LABEL = "Step";
+    private static final String RESUME_LABEL = "Resume";
+    private static final String PAUSE_LABEL = "Pause";
+    private static final String STOP_LABEL = "Stop";
 
     public InvocationPanel() {
         initialize();
     }
 
     private void initialize() {
-        var sort = UiFactory.createButton("Sort", () -> Controllers.fetch(SortController.class)
-                                                                   .sort());
-        var step = UiFactory.createButton("Step", () -> Controllers.fetch(SortController.class)
-                                                                   .step());
-        var resume = UiFactory.createButton("Resume", () -> Controllers.fetch(SortController.class)
-                                                                       .resume());
-        var pause = UiFactory.createButton("Pause", () -> Controllers.fetch(SortController.class)
-                                                                     .pause());
-        var stop = UiFactory.createButton("Stop", () -> Controllers.fetch(SortController.class)
-                                                                   .stop());
+        var sort = UiFactory.createButton(SORT_LABEL, () -> invokeControllerAction(SortController::sort));
+        var step = UiFactory.createButton(STEP_LABEL, () -> invokeControllerAction(SortController::step));
+        var resume = UiFactory.createButton(RESUME_LABEL, () -> invokeControllerAction(SortController::resume));
+        var pause = UiFactory.createButton(PAUSE_LABEL, () -> invokeControllerAction(SortController::pause));
+        var stop = UiFactory.createButton(STOP_LABEL, () -> invokeControllerAction(SortController::stop));
         sort.setSelected(true);
 
         Ui.using(this)
           .add(sort, step, resume, pause, stop)
           .get();
+    }
+
+    private void invokeControllerAction(Consumer<SortController> action) {
+        logger.trace("Invoking controller action in {}", getClass().getSimpleName());
+        action.accept(Controllers.fetch(SortController.class));
     }
 }
